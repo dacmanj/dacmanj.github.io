d7f54605ebf0f60bd56b5af6c623de96b8b2d403
David Manuel <dmanuel@pflag.org>
2016-07-09 13:22:53 -0400

    js

--- portfolio.js
+++ portfolio.js
@@ -10,13 +10,16 @@ function showPortfolioItem(e) {
 }
 
 $().ready(function() {
-   //add github icon to github links
-   $("a:contains('On Github')").html("<i class='fa fa-github' style='height: 18px;'></i>&nbsp;On Github");
-   //activate hash if linked directly
-   if (window.location.hash) { $("ul#portfolio-menu li a[href=" + window.location.hash + "]").parent().addClass("active"); $(window.location.hash).fadeIn(200);}
+  //add github icon to github links
+  $("a:contains('On Github')").html("<i class='fa fa-github' style='height: 18px;'></i>&nbsp;On Github");
    buildMenu();
    $(".portfolio").show();
 });
+
+function activateHash() {
+  //activate hash if linked directly
+  if (window.location.hash) { $("ul#portfolio-menu li a[href=" + window.location.hash + "]").parent().addClass("active"); $(window.location.hash).fadeIn(200);}
+}
 
 function buildMenu() {
     var menu = $("ul#portfolio-menu");
@@ -24,11 +27,12 @@ function buildMenu() {
     var items = $("div.portfolio");
     $(items).each(function(e) {
       console.log($(this));
-      console.log($(this).attr("id"));
-      console.log($("h2",this).text());
-      var new_menu_item = $("<li><a href='#" + $(this).attr("id") + "'>" + "test" + "</a></li>");
+      var id = $(this).attr("id");
+      var text = $("h2",this).text();
+      var new_menu_item = $("<li><a href='#" + is + "'>" + text + "</a></li>");
       menu.append(new_menu_item);
     })
+    activateHash();
 }
 
 /*
