997614e6f1a71d113f110b527c2043504ee19a23
David Manuel <dmanuel@pflag.org>
2016-07-11 22:49:22 -0400

    redirect

--- portfolio-redirect.js
+++ portfolio-redirect.js
@@ -4,6 +4,28 @@ switch(hash) {
   case "#openenrollment":
     redirect = "/tagged/portfolio/#/page-1/id-147267515253";
     break;
+  case "grouptextalert": //is hash correct
+    redirect = "/tagged/portfolio/#/page-1/id-147267569713";
+    break;
+  case "invoicing":
+    redirect = "/tagged/portfolio/#/page-1/id-147267612063";
+    break;
+  case "letterhead":
+    redirect = "/tagged/portfolio/#/page-1/id-147267761458";
+    break;
+  case "irssearch":
+    redirect = "/tagged/portfolio/#/page-1/id-147267335613";
+    break;
+  case "congressional-map":
+    redirect = "/tagged/portfolio/#/page-1/id-147267188198";
+    break;
+  case "sfe-website":
+    redirect = "/tagged/portfolio/#/page-1/id-147266818418";
+    break;
+    redirect = "/tagged/portfolio/#/page-1/id-147266640543";
+  case "":
+    redirect = "/tagged/portfolio/#/page-1/id-147266640543";
+    break;
   default:
     redirect = "/tagged/portfolio";
 
