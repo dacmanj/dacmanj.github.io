a7f1d768a4d9c3a0b04a4fc5f4d8f513b51b4631
David Manuel <dmanuel@pflag.org>
2016-07-11 22:42:53 -0400

    redirect

--- portfolio-redirect.js
+++ portfolio-redirect.js
@@ -1,13 +1,11 @@
 var hash=window.location.hash;
 var redirect = "";
-$().ready(function() {
-  switch(hash) {
-    case "#openenrollment":
-      redirect = "/tagged/portfolio/#/page-1/id-147267515253";
-      break;
-    default:
-      redirect = "/tagged/portfolio";
+switch(hash) {
+  case "#openenrollment":
+    redirect = "/tagged/portfolio/#/page-1/id-147267515253";
+    break;
+  default:
+    redirect = "/tagged/portfolio";
 
-  }
-  window.location = redirect;
-});
+}
+window.location = redirect;
